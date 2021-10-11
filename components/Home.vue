471d7f1270e0f337f37a003707dafc0dfc9ead45
samaratungajs <samaratungajs@gmail.com>
2021-10-11 20:22:55 +0530

    Movies component fixes #1

--- components/Home.vue
+++ components/Home.vue
@@ -1,224 +1,87 @@
 <template>
   <div>
     <div class="container search">
-       <input v-model.lazy="searchInput" type="text" placeholder="Search" @keyup.enter="$fetch" /> 
+      <input
+        v-model.lazy="searchInput"
+        type="text"
+        placeholder="Search"
+        @keyup.enter="$fetch"
+      />
     </div>
-
-     <div class="container movies">
-      <!-- searched grid -->
-      <div v-if="searchInput !== '' " id="movie-grid" class="movies-grid">
-        <div v-for="(movie,index) in searchedMovies" :key="index" class="movie" >
-          <div class="movie-img">
-            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt=""/>
-            <p class="review">{{movie.vote_average}}</p>
-            <!-- <p class="review">{{movie.overview}}</p> -->
-          </div>
-          <div class="info">
-            <p class="title">{{movie.title}}</p>
-            <p class="release">
-              Released:{{movie.release_date}}
-            </p>
-            <NuxtLink
-              class="button button-light"
-              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
-            >
-              View More
-            </NuxtLink>
-            <v-btn icon class="icn" :disabled="clicked.includes(movie.id)" @click="addToWatchlist(movie)">
-              <v-icon class="ic"  >mdi-bookmark-plus-outline</v-icon>
-            </v-btn>
-          </div>
-        </div>
-      </div>
-      <!-- latest grid -->
-      <div v-else id="movie-grid" class="movies-grid">
-        <div v-for="(movie,index) in movies" :key="index" class="movie" >
-          <div class="movie-img">
-            <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt=""/>
-            <p class="review">{{movie.vote_average}}</p>
-            <!-- <p class="review">{{movie.overview}}</p> -->
-          </div>
-          <div class="info">
-            <p class="title">{{movie.title}}</p>
-            <p class="release">
-              Released:{{movie.release_date}}
-            </p>
-            <NuxtLink
-              class="button button-light"
-              :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
-            >
-              View More
-            </NuxtLink>
-             <v-btn icon class="icn" :disabled="clicked.includes(movie.id)" @click="addToWatchlist(movie)">
-              <v-icon class="ic"  >mdi-bookmark-plus-outline</v-icon>
-            </v-btn>
-          </div>
-        </div>
-      </div>
-
-    </div>
+    <Movies
+      :movies="movies"
+      :searchedMovies="searchedMovies"
+      :searchInput="searchInput"
+    ></Movies>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
+import Movies from './Movies.vue'
 export default {
-    name: 'Home',
-     data() {
+  name: 'Home',
+  components: { Movies },
+  data() {
     return {
       movies: [],
       searchedMovies: [],
       searchInput: '',
-      wishlist:[],
-      clicked: []
+      wishlist: [],
+      clicked: [],
     }
   },
-  async fetch(){
-    if(this.searchInput ===''){
+  async fetch() {
+    if (this.searchInput === '') {
       await this.getMovies()
       this.searchInput = ''
       this.searchedMovies = []
       return
     }
-      await this.searchMovies()
+    await this.searchMovies()
   },
   methods: {
     async getMovies() {
-      const data = await this.$axios.$get(`https://api.themoviedb.org/3/movie/now_playing?api_key=fefd2c0c4f5fa3f7e9de5368cbbf75fd&language=en-US&page=1`)
+      const data = await this.$axios.$get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=fefd2c0c4f5fa3f7e9de5368cbbf75fd&language=en-US&page=1`
+      )
       data.results.forEach((movie) => {
         this.movies.push(movie)
       })
       // console.log(data)
     },
-    async searchMovies(){
-      const data = await this.$axios.$get(`https://api.themoviedb.org/3/search/movie?api_key=fefd2c0c4f5fa3f7e9de5368cbbf75fd&language=en-US&page=1&query=${this.searchInput}`)
+    async searchMovies() {
+      const data = await this.$axios.$get(
+        `https://api.themoviedb.org/3/search/movie?api_key=fefd2c0c4f5fa3f7e9de5368cbbf75fd&language=en-US&page=1&query=${this.searchInput}`
+      )
       data.results.forEach((movie) => {
         this.searchedMovies.push(movie)
       })
-   },
-   addToWatchlist(movie) {
-          this.clicked.push(movie.id)
-        this.$store.dispatch("wishlist/addToWishlist", movie);
-   }
-  }
+    },
+    addToWatchlist(movie) {
+      this.clicked.push(movie.id)
+      this.$store.dispatch('wishlist/addToWishlist', movie)
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-    .red {
-      color: red;
-    }
-
-  .search {
-    display: flex;
-    margin-left: 35%;
-    padding: 15px 16px;
-    input {
-      max-width: 350px;
-      width: 100%;
-      padding: 12px 6px;
-      font-size: 14px;
-      background: white;
-      outline: white;
-      border-radius: 10px;
-    }
-    .button {
-      border-top-left-radius: 0;
-      border-bottom-left-radius: 0;
-    }
+.search {
+  display: flex;
+  margin-left: 35%;
+  padding: 15px 16px;
+  input {
+    max-width: 350px;
+    width: 100%;
+    padding: 12px 6px;
+    font-size: 14px;
+    background: white;
+    outline: white;
+    border-radius: 10px;
   }
-  .movies {
-    padding: 25px 60px;
-    .movies-grid {
-      display: grid;
-      column-gap: 32px;
-      row-gap: 64px;
-      grid-template-columns: 1fr;
-      @media (min-width: 500px) {
-        grid-template-columns: repeat(2, 1fr);
-      }
-      @media (min-width: 750px) {
-        grid-template-columns: repeat(3, 1fr);
-      }
-      @media (min-width: 1100px) {
-        grid-template-columns: repeat(4, 1fr);
-      }
-      .movie {
-        position: relative;
-        display: flex;
-        flex-direction: column;
-        .movie-img {
-          position: relative;
-          overflow: hidden;
-          &:hover {
-            .overview {
-              transform: translateY(0);
-            }
-          }
-          img {
-            display: block;
-            width: 100%;
-            height: 100%;
-          }
-          .review {
-            position: absolute;
-            top: 0;
-            right: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 40px;
-            height: 40px;
-            background-color: #DAA520;
-            color: #fff;
-            border-radius: 0 0 15px 15px;
-            box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-              0 2px 4px -1px rgba(0, 0, 0, 0.06);
-          }
-          .overview {
-            line-height: 1.5;
-            position: absolute;
-            bottom: 0;
-            background-color: rgba(201, 38, 2, 0.9);
-            padding: 12px;
-            color: #fff;
-            transform: translateY(100%);
-            transition: 0.3s ease-in-out all;
-          }
-        }
-        .info {
-          margin-top: auto;
-          .title {
-            margin-top: 8px;
-            color: #fff;
-            font-size: 20px;
-          }
-          .release {
-            margin-top: 8px;
-            color: #c9c9c9;
-          }
-          .button {
-            margin-top: 8px;
-          }
-          .icn {
-            position: absolute;
-            bottom: 1%;
-            right: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center; 
-            &:disabled{
-             background-color: #c4e9fa9a;
-          }        
-            }
-          .ic{
-            color: #fff;
-            
-          }
-           
-        }
-      }
-    }
+  .button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
   }
-
+}
 </style>
---
+++ components/Movies.vue
@@ -0,0 +1,168 @@
+<template>
+  <div class="container movies">
+    <!-- movies grid -->
+    <div id="movie-grid" class="movies-grid">
+      <div v-for="(movie, index) in userComponent" :key="index" class="movie">
+        <div class="movie-img">
+          <img
+            :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+            alt=""
+          />
+          <p class="review">{{ movie.vote_average }}</p>
+        </div>
+        <div class="info">
+          <p class="title">{{ movie.title }}</p>
+          <p class="release">Released:{{ movie.release_date }}</p>
+          <NuxtLink
+            class="button button-light"
+            :to="{ name: 'movies-movieid', params: { movieid: movie.id } }"
+          >
+            View More
+          </NuxtLink>
+          <v-btn
+            icon
+            class="icn"
+            :disabled="clicked.includes(movie.id)"
+            @click="addToWatchlist(movie)"
+          >
+            <v-icon class="ic">mdi-bookmark-plus-outline</v-icon>
+          </v-btn>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Movies',
+  props: {
+    searchInput: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    movies: {
+      type: Array,
+      required: true,
+    },
+    searchedMovies: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      wishlist: [],
+      clicked: [],
+    }
+  },
+  computed: {
+    userComponent() {
+      if (this.searchInput !== '') {
+        return this.searchedMovies
+      } else return this.movies
+    },
+  },
+  methods: {
+    addToWatchlist(movie) {
+      this.clicked.push(movie.id)
+      this.$store.dispatch('wishlist/addToWishlist', movie)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.movies {
+  padding: 25px 60px;
+  .movies-grid {
+    display: grid;
+    column-gap: 32px;
+    row-gap: 64px;
+    grid-template-columns: 1fr;
+    @media (min-width: 500px) {
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media (min-width: 750px) {
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media (min-width: 1100px) {
+      grid-template-columns: repeat(4, 1fr);
+    }
+    .movie {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      .movie-img {
+        position: relative;
+        overflow: hidden;
+        &:hover {
+          .overview {
+            transform: translateY(0);
+          }
+        }
+        img {
+          display: block;
+          width: 100%;
+          height: 100%;
+        }
+        .review {
+          position: absolute;
+          top: 0;
+          right: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 40px;
+          height: 40px;
+          background-color: #daa520;
+          color: #fff;
+          border-radius: 0 0 15px 15px;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        }
+        .overview {
+          line-height: 1.5;
+          position: absolute;
+          bottom: 0;
+          background-color: rgba(201, 38, 2, 0.9);
+          padding: 12px;
+          color: #fff;
+          transform: translateY(100%);
+          transition: 0.3s ease-in-out all;
+        }
+      }
+      .info {
+        margin-top: auto;
+        .title {
+          margin-top: 8px;
+          color: #fff;
+          font-size: 20px;
+        }
+        .release {
+          margin-top: 8px;
+          color: #c9c9c9;
+        }
+        .button {
+          margin-top: 8px;
+        }
+        .icn {
+          position: absolute;
+          bottom: 1%;
+          right: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          &:disabled {
+            background-color: #c4e9fa9a;
+          }
+        }
+        .ic {
+          color: #fff;
+        }
+      }
+    }
+  }
+}
+</style>
